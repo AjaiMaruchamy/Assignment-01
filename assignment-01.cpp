6dd50126837d68950ef835532b093643bb868a3b
AjaiMaruchamy <127426329+AjaiMaruchamy@users.noreply.github.com>
2023-03-09 22:12:20 +0530

    Day 3 Assignment submitted

---
+++ assignment-01.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+	int M;
+	scanf("%d",&M);
+	if(M%3==0 &&M%5==0)
+	{
+		printf("Good Number");
+	}
+	else if(M%3==0 && M%5!=0)
+	{
+		printf("Bad Number");
+	}
+	else if(M%3!=0 && M%5!=0)
+	{
+		printf("Poor Number");
+	}
+	return 0;
+}
